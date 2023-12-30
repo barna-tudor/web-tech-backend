41af2c48f5173ce68e0648df9dd244ada35b58e0
barna-tudor <tudor.barna01@e-uvt.ro>
2023-12-30 15:29:00 +0200

    Update dbConfig.js

---
+++ src/database/dbConfig.js
@@ -0,0 +1,26 @@
+const dontenv = require(dontenv).config();
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'postgres',
+    host: process.env.DB_HOST,
+    database: process.env.DB,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    idleTimeoutMillis: 60 * 1000,
+    max: 5,
+})
+
+pool.on('connect', async (client) => {
+    try {
+        await client.query(`SET search_path TO ${process.env.DB_SCHEMA};`)
+        console.log('Pool Client Connect');
+    } catch (e) {
+        console.error('Error setting search_path: ', error)
+    }
+})
+
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client:', err)
+})
+
+module.exports = { pool }
