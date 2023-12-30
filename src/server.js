f48182f32bd3e1537283c31b634dda7eb99ed3e7
barna-tudor <tudor.barna01@e-uvt.ro>
2023-12-30 15:37:45 +0200

    basic server.js setup

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const cors = require('cors');
+app.use(cors({
+    origin: process.env.FRONT_END
+}))
+const helmet = require('helmet');
+app.use(helmet());
+
+//routes:
+// const threadsRouter = require('./routes/threads');
+// const topicsRouter = require('./routes/topics');
+// const usersRouter = require('./routes/users');
+// app.use('/api', threadsRouter);
+// app.use('/api', topicsRouter);
+// app.use('/api', usersRouter);
+
+app.listen(PORT, () => {
+    console.log('Listening on port ', PORT);
+})
