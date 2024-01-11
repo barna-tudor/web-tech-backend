b9c0c9ff48b2177295b4c3723404676e350e92aa
barna-tudor <tudor.barna01@e-uvt.ro>
2024-01-11 15:49:07 +0200

    no message

---
+++ src/database/queries/users.js
@@ -0,0 +1,26 @@
+const registerUserQuery =
+    `INSERT INTO "user"(username, display_name, email, password)
+    VALUES($1,$2,$3,$4)`;
+
+const checkUsernameExists = 
+    `SELECT COUNT(*)
+    FROM "user"
+    WHERE username = $1`
+
+const getUserByLoginQuery =
+    `SELECT user_id, display_name, join_date
+    FROM "user"
+    WHERE (email = $1 AND password = $2) OR (username = $1 AND password = $2)
+    `
+
+const checkEmailQuery =
+    `SELECT COUNT(*)
+    FROM "user"
+    WHERE email = $1
+    `
+
+const getUserByUsernameQuery =
+    `SELECT display_name, email, join_date
+    FROM "user"
+    WHERE username = $1
+    `
